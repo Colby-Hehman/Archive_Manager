86d0c4b68e70a2d83851752512d0ca5e29d696fd
Colby-Hehman <76548385+Colby-Hehman@users.noreply.github.com>
2021-10-14 00:27:04 -0400

    Optimization
    I added several optimizations that will make the code run much faster than before. Now most fetching of data will be inside of the __init__ methods for the File and Directory classes. Before now this data had been gathered each time its method was called (several times) instead of just once inside of __init__. The code has also been written to be much more readable

--- folder_comparison_and_copy.py
+++ folder_comparison_and_copy.py
@@ -14,11 +14,13 @@ class File:
         if "/" in self.path:
             self.path = path.replace("/", "\\")
 
+        self.size = os.stat(self.path).st_size
+
     def get_path(self):
         return self.path
 
     def get_size(self):
-        return os.stat(self.path).st_size
+        return self.size
 
     def delete_file(self):
         send2trash(self.path)
@@ -31,42 +33,65 @@ class Directory:
         if "/" in self.path:
             self.path = path.replace("/", "\\")
 
+        try:
+            self.subdir_names = [f for f in os.listdir(self.path) if isdir(join(self.path, f)) if f != "desktop.ini"]
+        except PermissionError:
+            self.subdir_names = []
+        except FileNotFoundError:
+            self.subdir_names = []
+
+        self.subdirs = [self.path + "\\" + f for f in self.subdir_names]
+        if self.subdirs == []:
+            self.subdirs = None
+
+        try:
+            self.file_names = [f for f in listdir(self.path) if isfile(join(self.path, f)) if f != "desktop.ini"]
+        except PermissionError:
+            self.file_names = [][1:]
+        except FileNotFoundError:
+            self.file_names = [][1:]
+
+        self.files = [self.path + "\\" + f for f in self.file_names]
+
+        if self.subdirs != None:
+            self.subdir_num = len(self.subdirs)
+        else:
+            self.subdir_num = 0
+
+
     def get_path(self):
         return self.path
 
     def get_subdirs(self):
         # Returns a list of each subdirectory path
-        subdirs = [self.path + "\\" + f for f in os.listdir(self.path) if isdir(join(self.path, f))]
-        if subdirs == []:
-            return None
-        return subdirs
+        return self.subdirs
 
     def subdir_ends(self):
         # returns a list of each
-        return [f for f in os.listdir(self.path) if isdir(join(self.path, f))]
+        return self.subdir_names
 
     def num_of_subdirs(self):
-        subdirs = self.get_subdirs()
-        if subdirs == None:
-            return 0
-        else:
-            return len(subdirs)
+        return self.subdir_num
 
     def get_files(self):
-        # Returns a list of all files inside the directory
-        files = [self.path + "\\" + f for f in listdir(self.path) if isfile(join(self.path, f))]
-        if "desktop.ini" in files:
-            files = files[1:]
-        return files
+        # Returns a list of file paths inside the directory
+        return self.files
+
+    def file_ends(self):
+        # Returns a list of file names inside the directory
+        return self.file_names
 
     def delete_dir(self):
-        send2trash(self.path)
+        try:
+            send2trash(self.path)
+        except OSError:
+            pass
 
 
 # This is used rarely, when we need to empty out the contents of and entire folder (and its subfolders) into one place
 # This is all done recursively. Parameters taken in as objects
 def empty_subdirs(to_empty, destination):
-    # print(f"empty_subdirs         {to_empty.get_path()}  |  {destination.get_path()}")
+    print(f"empty_subdirs       {to_empty.get_path()}  |  {destination.get_path()}")
 
     if to_empty.get_subdirs():
         if to_empty.num_of_subdirs() > 1:
@@ -79,15 +104,17 @@ def empty_subdirs(to_empty, destination):
         try:
             os.rename(file, destination.get_path() + file[file.rindex("\\"):])
         except FileExistsError:
-            file = File(file)
-            file.delete_file()
+            File(file).delete_file()
+        except FileNotFoundError:
+            pass
+
     to_empty.delete_dir()
 
 
 # When this function is called, it will copy over every file in a directory from model_folder to to_change
 # Extra files in to_change but not model_folder will be deleted. Parameters are taken as Directory class objects
 def copy_files(to_change, model_folder):
-    # print(f"copy_files          {to_change.get_path()}  |  {model_folder.get_path()}")
+    print(f"copy_files          {to_change.get_path()}  |  {model_folder.get_path()}")
 
     # This is where we check to see if there are any subdirectories in to_change that are not in model_folder
     # If there are, then we empty and delete them by calling empty_subdirs() above
@@ -103,36 +130,41 @@ def copy_files(to_change, model_folder):
 
     # Here we will iterate through every file in to_change. If a file has a matching filepath and byte size it
     # is left alone. If not, it is deleted.
-    model_folder_files = [file[file.rindex("\\") + 1:] for file in model_folder.get_files()]
-    for file in to_change.get_files():
-        if file[file.rindex("\\") + 1:] not in model_folder_files:
-            File(file).delete_file()
+    for file in to_change.file_ends():
+        if file not in model_folder.file_ends():
+            File(to_change.get_path() + "\\" + file).delete_file()
 
     # Then, we iterate through every file in model_folder.
     # If there is not a matching filepath in to_change the file is copied over
-    to_change_files = [file[file.rindex("\\") + 1:] for file in to_change.get_files()]
-    for file in model_folder.get_files():
+    # The try-except blocks are because files may not copy over without permission, or if the size is 0 bytes
+    for file in model_folder.file_ends():
 
-        if file[file.rindex("\\") + 1:] in to_change_files:
-            to_model_file = File(file)
-            to_change_file = File(to_change.get_path() + file[file.rindex("\\"):])
-            if to_model_file.get_size() == to_change_file.get_size():
-                continue
-            else:
+        if file in to_change.file_ends():
+            to_model_file = File(model_folder.get_path() + "\\" + file)
+            to_change_file = File(to_change.get_path() + "\\" + file)
+            if to_model_file.get_size() != to_change_file.get_size():
                 to_change_path = to_change_file.get_path()
                 to_change_file.delete_file()
-                shutil.copyfile(to_model_file.get_path(), to_change_path)
+                try:
+                    shutil.copyfile(to_model_file.get_path(), to_change_path)
+                except PermissionError:
+                    pass
+                except OSError:
+                    pass
         else:
-            shutil.copyfile(file, to_change.get_path() + file[file.rindex("\\"):])
-
-
+            try:
+                shutil.copyfile(model_folder.get_path() + "\\" + file, to_change.get_path() + "\\" + file)
+            except PermissionError:
+                pass
+            except OSError:
+                pass
 
 
 # Here is a function that will copy over an entire folder from scratch. It recurses through each level of
 # subdirectories until it finds a directory without any subdirectories. Then each folder is copied over
 # Both parameters take in file paths as strings
 def copy_entire_folder(to_change, model_folder):
-    # print(f"copy_entire_folder  {to_change.get_path()}  |  {model_folder.get_path()}")
+    print(f"copy_entire_folder  {to_change}  |  {model_folder}")
 
     to_change, model_folder = Directory(to_change), Directory(model_folder)
     if model_folder.get_subdirs():
@@ -143,21 +175,16 @@ def copy_entire_folder(to_change, model_folder):
     copy_files(Directory(to_change.get_path()), Directory(model_folder.get_path()))
 
 
-
-
 # Here is a recursive function that will call copy_folders() on any folder at the lowest level in model_folder,
 # and then call copy_folders() on every function coming back up
 def traverse_folders(folder_to_change, model_folder):
-
     to_change = Directory(folder_to_change)
     model_folder = Directory(model_folder)
     if model_folder.get_subdirs():
 
         if to_change.get_subdirs():
-            to_change_subdirs = [dir[dir.rindex("\\") + 1:] for dir in to_change.get_subdirs()]
-            model_subdirs = [dir[dir.rindex("\\") + 1:] for dir in model_folder.get_subdirs()]
-            for dir in model_subdirs:
-                if dir in to_change_subdirs:
+            for dir in model_folder.subdir_ends():
+                if dir in to_change.subdir_ends():
                     traverse_folders(to_change.get_path() + "\\" + dir, model_folder.get_path() + "\\" + dir)
                 else:
                     new_path = to_change.get_path() + "\\" + dir
@@ -179,3 +206,4 @@ def traverse_folders(folder_to_change, model_folder):
 # 'C:\\Users\\Colby Hehman\\Pictures\\Backups\\Camera Roll Backup')
 # Remember that the folder_to_change (first parameter) will change to be a copy of the model_folder (second parameter)
 traverse_folders('', '')
+
